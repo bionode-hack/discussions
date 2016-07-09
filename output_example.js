b05cccd5581de034f4295fa6a5a629e28881f59e
Adrian Alexa <adrian@repositive.io>
2016-07-09 11:16:43 +0100

    Create output_example.js

---
+++ output_example.js
@@ -0,0 +1,9 @@
+{
+  external_id : "E-GEOD-10233",
+  title : "Transcription profiling by array ...",
+  description : "Transcription profiling by array of human PMA-activated MM6 cells ...",
+  tech : "A-AFFY-44",
+  assay : "Expression-Array",
+  url : "https://www.ebi.ac.uk/gxa/experiments/E-GEOD-10233",
+  access : "open"
+}
